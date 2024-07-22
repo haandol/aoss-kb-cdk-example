5e7950f37cbc2434ded2048ad859173eed1a6a4a
haandol <ldg55d@gmail.com>
2024-07-23 08:40:21 +0900

    fix: apply ns to name of stack

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -4,9 +4,14 @@ import * as cdk from 'aws-cdk-lib';
 import { EmbeddingStack } from '../lib/embedding-stack';
 import { Config } from '../config/loader';
 
-const app = new cdk.App();
+const app = new cdk.App({
+  context: {
+    ns: Config.app.ns,
+    stage: Config.app.stage,
+  },
+});
 
-new EmbeddingStack(app, 'EmbeddingStack', {
+new EmbeddingStack(app, `${Config.app.ns}Embedding`, {
   knowledgeBaseName: Config.knowledgeBase.name,
   dataSourceName: Config.knowledgeBase.dataSourceName,
 });
